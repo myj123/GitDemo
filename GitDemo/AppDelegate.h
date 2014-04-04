ac5bd52a44acfa40d85c744989a420421d449854
吴楠 <geek5nan@gmail.com>
2014-04-04 14:55:37 +0800

    Initial Commit

---
+++ GitDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by cora1n on 14-4-4.
+//  Copyright (c) 2014年 devwu.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GitDemo
+//
+//  Created by cora1n on 14-4-4.
+//  Copyright (c) 2014年 devwu.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
