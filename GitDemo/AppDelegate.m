e1787d6fe0eafb4528418a7f22adb473712101a2
吴楠 <geek5nan@gmail.com>
2014-04-04 15:35:43 +0800

    add 11111111111

--- GitDemo/AppDelegate.m
+++ GitDemo/AppDelegate.m
@@ -19,8 +19,17 @@
     NSLog(@"删除空行,并添加一行");
 
 
-    //A修改了该文件1111111111
-    
+    //A修改了该文件111111111111
+    /**
+     *  To https://github.com/geek5nan/GitDemo.git
+     ! [rejected]        master -> master (fetch first)
+     error: failed to push some refs to 'https://github.com/geek5nan/GitDemo.git'
+     hint: Updates were rejected because the remote contains work that you do
+     hint: not have locally. This is usually caused by another repository pushing
+     hint: to the same ref. You may want to first integrate the remote changes
+     hint: (e.g., 'git pull ...') before pushing again.
+     hint: See the 'Note about fast-forwards' in 'git push --help' for details.
+     */
 
     return YES;
 }
